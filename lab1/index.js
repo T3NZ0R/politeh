2960653aca9f3592270d2db7fe0d93bc125a8d40
T3NZ0R <t3nz000r@gmail.com>
2022-02-17 14:37:55 +0200

    Initial commit

---
+++ lab1/index.js
@@ -0,0 +1,21 @@
+let processes = [];
+
+let numberOfProcesses = Math.floor(Math.random() * (10 - 5) + 5);
+
+for (let i = 0; i <numberOfProcesses; i++) {
+    processes[i] = Math.floor(Math.random() * (30 - 3) + 3);
+    console.log(processes[i]);
+}
+
+let WT = [0,];
+let sumWT = 0;
+
+for (let i = 0; i <=numberOfProcesses ; i++) {
+    WT[i+1] = processes[i] + WT[i];
+    console.log(WT[i]);
+    sumWT = sumWT + WT[i];
+}
+
+let avrWT = sumWT/numberOfProcesses;
+console.log(avrWT);
+
