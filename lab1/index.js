d7bbc248f1214e493c86fadc781b74669ffc20a7
T3NZ0R <t3nz000r@gmail.com>
2022-04-07 00:13:54 +0300

    new

--- lab1/index.js
+++ lab1/index.js
@@ -1,24 +1,51 @@
 let processes = [];
 
-let numberOfProcesses = Math.floor(Math.random() * (10 - 5) + 5);
+let numberOfProcesses = 5;
 
-for (let i = 0; i <numberOfProcesses; i++) {
+for (let i = 0; i < numberOfProcesses; i++) {
     processes[i] = Math.floor(Math.random() * (30 - 3) + 3);
-    console.log(processes[i]);
 }
+console.log("Processes:");
+console.log(processes);
+
+console.log("---------------------------------");
+//-------------------------------------------------------------------------------------------
 
 let WT = [0];
 let sumWT = 0;
 
-for (let i = 0; i <=numberOfProcesses ; i++) {
-    WT[i+1] = processes[i] + WT[i];
-    console.log(WT[i]);
+for (let i = 0; i <= numberOfProcesses - 1; i++) {
+
+    WT[i + 1] = processes[i] + WT[i];
     sumWT = sumWT + WT[i];
 }
 
-let avrWT = sumWT/numberOfProcesses;
-console.log(avrWT);
+let avrWT = (sumWT / numberOfProcesses).toFixed(1);
+console.log("FCFS algorithm");
+console.log(WT);
 
-let processesSorted = processes.sort((number1, number2)=>number1 - number2);
+console.log("Result = " + avrWT);
 
+console.log("---------------------------------");
+//-------------------------------------------------------------------------------------------
+
+let processesSorted = processes.sort((number1, number2) => number1 - number2);
+console.log("SJF algorithm");
 console.log(processesSorted);
+
+sumWT = 0;
+
+for (let i = 0; i <= processesSorted.length-1; i++) {
+    sumWT = sumWT + processesSorted[i];
+}
+
+avrWT = (sumWT / numberOfProcesses).toFixed(1);
+
+console.log("Result = " + avrWT);
+console.log("---------------------------------");
+
+//-------------------------------------------------------------------------------------------
+
+
+
+
